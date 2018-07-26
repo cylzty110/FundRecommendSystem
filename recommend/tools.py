83af7ba52c42b495becafea5ac050e735b2c2a18
David Cui <694894781@qq.com>
2018-07-26 22:06:53 +0800

    1.将数据库数据转换为DataFrame数据

--- recommend/tools.py
+++ recommend/tools.py
@@ -1,7 +1,23 @@
 import json
+import pandas as pd
+from recommend.custom import CustomMessage
+from recommend import custom, models
 
 
-#类转Json
+# 类转Json
 def classToJson(obj):
     json_str = json.dumps(obj, default=lambda o: o.__dict__, ensure_ascii=False, indent=2)
     return json_str
+
+# 将数据库数据转换为DataFrame类型数据
+def getData():
+    dataList = []
+    userList = models.TBL_CUST_ID_CONV.selectAll()
+    for userId in userList:
+        customMessage = CustomMessage.selectByEcifId(userId.ECIF_CST_ID)
+        for dic in customMessage.__dict__:
+            temp = getattr(customMessage, dic)
+            for key in temp.__dict__:
+                dataList.append(getattr(temp, key))
+    dataFrame = pd.DataFrame(dataList)
+    return dataFrame
