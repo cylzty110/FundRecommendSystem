046f2b6a8fdc7df0fd8db094e387495ffc57025b
David Cui <694894781@qq.com>
2018-07-27 19:25:04 +0800

    1.encode.py完成 2.聚类

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
-from recommend import app, models, db, recommend,tools
+from recommend import app, recommend, encode
 from recommend.custom import CustomMessage
-from recommend.cluster import hierarchicalClustering
+from recommend.cluster import AgglomerativeClustering
+from recommend.tools import getData
 from flask import render_template
 from flask import request
 import pandas as pd
@@ -21,16 +22,30 @@ import numpy as np
 import pandas as pd
 from recommend.cluster import hierarchicalClustering
 
-a = hierarchicalClustering  ()
-a = pd.DataFrame(a)
-data12 = np.array(a[0].T, dtype=np.float)
-bins = [0, 2000, 8000, 20000, 50000, 100000]
 
-labels = ['究极贫困人口', '一般贫困人口', '普通人口', '较富有人口', '富有人口']
 
-data = pd.cut(data12, bins,right= False, labels = labels)
-adwq = data
-
+# a = hierarchicalClustering  ()
+# a = pd.DataFrame(a)
+# data12 = np.array(a[0].T, dtype=np.float)
+# bins = [0, 2000, 8000, 20000, 50000, 100000]
+#
+# labels = ['究极贫困人口', '一般贫困人口', '普通人口', '较富有人口', '富有人口']
+#
+# data = pd.cut(data12, bins,right= False, labels = labels)
+# adwq = data
+data = getData()
+maxnum = data.max()
+dataArray = np.empty(shape=[len(data), 0])
+for i in range(24):
+    if i in [4, 17, 18, 21, 22]:
+        temp = encode.onehotencode(data[i])
+    else:
+        temp = encode.intervalencode(data[i], [0, maxnum.get(i)/2, maxnum.get(i) + 1], ['0', '1'])
+        temp = temp.ravel()
+        temp = temp.reshape((1000, 1))
+    dataArray = np.concatenate((dataArray, temp), axis=1)
+cluster = AgglomerativeClustering(n_clusters=3, linkage='average').fit(dataArray)
+label = cluster.labels_
 
 # if __name__ == '__main__':
 #     app.run()
--- recommend/encode.py
+++ recommend/encode.py
@@ -3,15 +3,13 @@ import numpy as np
 from sklearn import preprocessing
 from recommend.cluster import hierarchicalClustering
 
-# 要求传输的是一个列向量 类型为pd.DataFrame
-def intervalencode(data):
 
-    data = np.array(data.T, dtype=np.float)
-    bins = [0, 2000, 8000, 20000, 50000, 100000]
-    labels = ['0', '1', '2', '3', '4']
-    data = pd.cut(data, bins, right=False, labels=labels)
-    result = np.array(data)
+# 要求传输的是一个列向量 类型为pd.DataFrame
+def intervalencode(data, bins, labels):
+    data = np.array(data, dtype=np.float)
+    result = pd.cut(data, bins, right=False, labels=labels)
     return result
+
 
 # 要求传输的是一个列向量 类型为pd.DataFrame
 def onehotencode(data):
